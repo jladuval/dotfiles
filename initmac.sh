f6c162ba980e20dfb0f75beafe2c36c862758315
Jacob Duval <jladuval@gmail.com>
2017-11-10 11:34:11 +0100

    some dotfiles

---
+++ initmac.sh
@@ -0,0 +1,10 @@
+cp ~/.bashrc ~/dotfiles/macos/.bashrc;
+cp ~/.config/ranger/rc.conf ~/dotfiles/macos/rc.conf;
+cp ~/.config/nvim/init.vim ~/dotfiles/macos/nvim/init.vim;
+cp ~/.fzf.zsh ~/dotfiles/macos/fzf/.fzf.zsh;
+cp ~/.gitconfig ~/dotfiles/macos/gitconfig;
+cp -a ~/.tmux/. ~/dotfiles/macos/tmux/;
+cp ~/.tmux.conf ~/dotfiles/macos/.tmux.conf;
+cp ~/.vimrc ~/dotfiles/macos/.vimrc;
+cp ~/.zshrc ~/dotfiles/macos/.zshrc;
+
