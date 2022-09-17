d74b5a14d6c89bc7b2b31ed8565ec133c8154fcb
Jacob Duval <jladuval@gmail.com>
2022-09-18 09:23:40 +1000

    fix multiple lsp server issue

--- apps/nvim/lua/plugins.lua
+++ apps/nvim/lua/plugins.lua
@@ -5,18 +5,18 @@ vim.call('plug#begin', '~/.local/share/nvim/plugged')
 if vim.g.vscode then
 else
   Plug 'jparise/vim-graphql' -- Graphql highlighting
-  Plug ('styled-components/vim-styled-components', { branch = 'main' }) -- Styled components highlighting
-  Plug 'ianks/vim-tsx' -- TSX support
-  Plug 'Quramy/vim-js-pretty-template' -- highlights graphql and html in template strings
+  -- Plug ('styled-components/vim-styled-components', { branch = 'main' }) -- Styled components highlighting
+  -- Plug 'ianks/vim-tsx' -- TSX support
+  -- Plug 'Quramy/vim-js-pretty-template' -- highlights graphql and html in template strings
 
   -- LSP
+  Plug 'nvim-lua/plenary.nvim' -- required for telescope and null-ls
   Plug 'neovim/nvim-lspconfig' -- required for the below plugins like prettier
   Plug 'jose-elias-alvarez/null-ls.nvim' -- base plugin for other plugins to use LSP
   Plug 'MunifTanjim/prettier.nvim' -- prettier support
 
   Plug ('nvim-treesitter/nvim-treesitter', {["do"]= ':TSUpdate'}) -- helps plugins do syntax tree highlighting
   Plug 'kyazdani42/nvim-web-devicons' -- devicons have colours now
-  Plug 'nvim-lua/plenary.nvim' -- required for telescope
   Plug ('nvim-telescope/telescope.nvim', { tag= '0.1.0' }) -- fuzzy finder
 
   Plug 'scrooloose/nerdtree' -- Nerdtree
@@ -59,22 +59,10 @@ if not vim.g.vscode then
       end
     end,
   })
+
   local prettier = require("prettier")
 
   prettier.setup({
-    ["null-ls"] = {
-      condition = function()
-        return prettier.config_exists({
-          -- if `true`, checks `package.json` for `"prettier"` key
-          check_package_json = false,
-        })
-      end,
-      runtime_condition = function(params)
-        -- return false to skip running prettier
-        return true
-      end,
-      timeout = 5000,
-    },
     bin = 'prettier', -- or `'prettierd'` (v0.22+)
 
     filetypes = {
@@ -96,26 +84,14 @@ if not vim.g.vscode then
   -- LUALINE
   require('lualine').setup()
 
-  -- LSP
-  local status, nvim_lsp = pcall(require, "lspconfig")
-  if (not status) then return end
-
-  local protocol = require('vim.lsp.protocol')
-
-  local on_attach = function(client, bufnr)
-    -- format on save
-    if client.server_capabilities.documentFormattingProvider then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("Format", { clear = true }),
-        buffer = bufnr,
-        callback = function() vim.lsp.buf.formatting_seq_sync() end
-      })
-    end
-  end
-
   -- TypeScript ( first run npm i -g typescript-language-server )
+  local nvim_lsp = require("lspconfig")
+
   nvim_lsp.tsserver.setup {
-    on_attach = on_attach,
+    on_attach = function(client)
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+    end,
     filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
     cmd = { "typescript-language-server", "--stdio" }
   }
